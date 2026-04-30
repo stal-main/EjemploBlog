c2c39b2982b4ea5111ef5d85040cab36f58bb512
Kevin Solano <7stalyakov7@gmail.com>
2026-04-30 02:40:14 -0600

    Clase controladora

--- src/control/Controladora.java
+++ src/control/Controladora.java
@@ -55,7 +55,54 @@ public class Controladora {
 		b.crearPublicacion(titulo, texto, nombre);
 	}
 	
-	public Map<Integer, String> obtenerPublicaciones(codigoBlog) {
+	public Map<Integer, String> obtenerPublicaciones(int codigoBlog) throws Exception {
+		
+		if (!blogs.containsKey(codigoBlog)) {
+			
+			throw new Exception("El codigo del blog no es valido");
+		}
+		
+		Blog b = blogs.get(codigoBlog);
+		
+		return b.obtenerTitulosPublicaciones();
+		
+	}
+	
+	public String obtenerPublicacion(int codigoBlog, int codigoPublicacion) throws Exception {
+		
+		if (!blogs.containsKey(codigoBlog)) {
+			
+			throw new Exception("El codigo del blog no es valido");
+		}
+		
+		Blog b = blogs.get(codigoBlog);
+		
+		return b.obtenerPublicacion(codigoPublicacion);
+		
+	}
+	
+	public void agregarComentario(int codigoBlog, int codigoPublicacion, String email, String ip, String texto) throws Exception {
+		
+		if (!blogs.containsKey(codigoBlog)) {
+			
+			throw new Exception("El codigo del blog no es valido");
+		}
+		
+		Blog b = blogs.get(codigoBlog);
+		
+		b.agregarComentario(codigoPublicacion, email, ip, texto);
+	}
+	
+	public void borrarComentario(int codigoBlog, int codigoPublicacion, int posicion) throws Exception {
+		
+		if (!blogs.containsKey(codigoBlog)) {
+			
+			throw new Exception("El codigo del blog no es valido");
+		}
+		
+		Blog b = blogs.get(codigoBlog);
+		
+		b.borrarComentario(codigoPublicacion, posicion);
 		
 	}
 }
